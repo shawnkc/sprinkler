6b6c8741a677c6bba361f6772857e96420a17952
Shawn <shawn.casey@gmail.com>
2012-12-09 10:13:18 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprinker::Application.config.secret_token = 'bf27d7c11cbc925f7092642ac573c679fd627a6e33e97835e35f95035ff84da8b774e2b8952cf5d2d18fb539aba1dd2f47101bb50811246f58118021b6af8d75'
