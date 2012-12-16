535df9b16067afd7e533beca9279410cc7046324
Shawn <shawn.casey@gmail.com>
2012-12-16 12:52:55 -0600

    start/stop/moisturre all work to db

--- app/controllers/sprinkler_controller.rb
+++ app/controllers/sprinkler_controller.rb
@@ -3,23 +3,36 @@
 class SprinklerController < ApplicationController
 
   # called when the sprinkler zone is turned on
-  # parms: duration, zone, moisture level
   def start
     # just logs the moisture value for now, 
     # time information is logged in stop
+    if (params.has_key?(:moisture))  
+      @moistureEntry = Moisture.new(:value => params[:moisture])
+      @moistureEntry.save
+    end
   end
 
+  # /stop?zone=1-6&duration=123&moisture=1.23
   def stop
+    if (params.has_key?(:moisture))  
+      @moistureEntry = Moisture.new(:value => params[:moisture])
+      @moistureEntry.save
+    end
+    if (params.has_key?(:zone) && params.has_key?(:duration))  
+      @zoneEntry = ZoneEntry.new(:zone => params[:zone], :duration => params[:duration])
+      @zoneEntry.save
+    end
   end
 
   # for stand alone moisture data updates
   def moisture
-    if (params.has_key?(:value))  
-      @moistureEntry = Moisture.new(:value => params[:value])
+    if (params.has_key?(:moisture))  
+      @moistureEntry = Moisture.new(:value => params[:moisture])
       #@moistureEntry.save
 
       @moistures = Moisture.all
 
+      @title="af"
       @test = Array.new()
       @values = Array.new()
       @x = Array.new()
