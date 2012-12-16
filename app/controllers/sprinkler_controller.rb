a7bfce96de89fd6b012250bbf260e9e8b458fa1c
Shawn <shawn.casey@gmail.com>
2012-12-16 16:41:49 -0600

    removed chart from moisture controller

--- app/controllers/sprinkler_controller.rb
+++ app/controllers/sprinkler_controller.rb
@@ -28,34 +28,7 @@ class SprinklerController < ApplicationController
   def moisture
     if (params.has_key?(:moisture))  
       @moistureEntry = Moisture.new(:value => params[:moisture])
-      #@moistureEntry.save
-
-      @moistures = Moisture.all
-
-      @title="af"
-      @test = Array.new()
-      @values = Array.new()
-      @x = Array.new()
-      @moistures.each do |x|
-        @values.push(x[:value].to_f)
-        @x.push(x[:created_at])
-        a = Array.new()
-        a.push(x[:created_at].to_time)
-#        a.push(x[:value].to_f)
-        a.push(x[:value].to_f)
-        @test.push(a)
-      end
-
-      @h = LazyHighCharts::HighChart.new('graph') do |f|
-        #f.title(:text=>'tmp')
-        f.options[:chart][:defaultSeriesType] = "line"
-        f.series(:name=>'voltage', :data=>@values)
-        #f.series(:name=>'John', :data=>[3, 20, 3, 5, 4, 10, 12 ,3, 5,6,7,7,80,9,9])
-        #f.series(:name=>'Jane', :data=> [1, 3, 4, 3, 3, 5, 4,-46,7,8,8,9,9,0,0,9] )
-#        f.xAxis(:title=>'Time')
-        #f.xAxis(:type=>'datetime', :labels=>@x)
-      end
-
+      @moistureEntry.save
     end
   end
 
